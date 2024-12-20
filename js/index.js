5ace233bf876a0e1fdf4d7779f17ac16898f7ec4
alizaiter777 <alizaiter777@gmail.com>
2024-12-20 15:53:48 +0200

    Implemented Js <> Prifix function

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ let final=[];
 
 
 rslt=document.getElementById('result');
-rslt.addEventListener('click',Post);
+rslt.addEventListener('click',Pri);
 
 box=document.getElementsByClassName('.box')
 boxes = document.querySelectorAll('.in-row:not(.exclude)');
@@ -36,6 +36,7 @@ log.innerHTML = 0;
 function Delete(){
     final.pop();
     screen.innerHTML=final.join(' ');
+    log.innerHTML = 0;
     console.log(final);       
     }
 
@@ -93,3 +94,54 @@ function Post() {
     }
     
    
+function Pri() {
+    let stack2 = [];
+    console.log(final); 
+    
+    for (let i = final.length - 1; i >= 0; i--) {
+        let index2 = final[i];
+    
+        if (!isNaN(index2)) {
+            stack2.push(parseFloat(index2));
+        } 
+        else {
+            let a = stack2.pop();
+            let b = stack2.pop();
+            let result;
+    
+           if (index2 === '+') {
+             result = a + b;
+            }
+           else if (index2 === '-') {
+             result = a - b;
+            } 
+            else if (index2 === 'x') {
+             result = a * b;
+            } 
+            else if (index2 === '/') {
+                if (b === 0) {
+                    console.error("division by zero");
+                    log.innerHTML = "error";
+                    return;
+                }
+             result = a / b;
+            } 
+           stack2.push(result);
+        }
+    }
+
+        if (stack2.length === 1) {
+            if(isNaN(stack2[0])){
+               log.innerHTML = "error"
+            }
+
+            else{
+               log.innerHTML = stack2[0]
+            }
+        } 
+        else {
+           log.innerHTML = "error";
+        }
+}
+    
+    
